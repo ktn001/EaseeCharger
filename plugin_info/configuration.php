a39bb1a9d2461023bee9ba73bafe8d760a086f53
ktn001 <X>
2022-06-25 22:42:32 +0200

    Option pour les log non sécurisés

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -18,8 +18,8 @@
 require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 include_file('core', 'authentification', 'php');
 if (!isConnect()) {
-  include_file('desktop', '404', 'php');
-  die();
+    include_file('desktop', '404', 'php');
+    die();
 }
 include_file('core', 'EaseeCharger', 'class', 'EaseeCharger');
 
@@ -27,27 +27,43 @@ $defaultPort = config::getDefaultConfiguration('EaseeCharger')['EaseeCharger']['
 ?>
 
 <form class="form-horizontal">
-  <fieldset>
     <div class="form-group">
-      <div class='col-sm-6'> <!-- partie gauche -->
+	<div class='col-md-6 col-sm-12'> <!-- partie gauche -->
 
-        <legend class="col-sm-12"><i class="fas fa-university"></i> {{Démon}}:</legend>
+	    <legend ><i class="fas fa-university"></i> {{Démon}}:</legend>
 
-        <label class="col-sm-2 control-label">
-          {{Port}}
-          <sup><i class="fas fa-question-circle" title="{{Redémarrer le démon en cas de modification}}"></i></sup>
-        </label>
-        <input class="configKey form-control col-sm-2" data-l1key="daemon::port" placeholder="<?php echo $defaultPort ?>"/>
+	    <fieldset>
+		<div class="row">
+		    <label class="col-sm-3 col-md-5 col-lg-4 control-label">
+			{{Port}}
+			<sup><i class="fas fa-question-circle" title="{{Redémarrer le démon en cas de modification}}"></i></sup>
+		    </label>
+		    <input class="configKey col-sm-2 form-control" data-l1key="daemon::port" placeholder="<?= $defaultPort ?>"/>
+		</div>
 
-	<label class="col-sm-4 control-label">
-	  {{Debug étendu}}
-	  <sup><i class="fas fa-question-circle" title="{{Niveau debug étendu pour le démon (très verbeux)}}"></i></sup>
-	</label>
-	<input class="configKey form-control" type="checkbox" data-l1key="extendedDebug"/>  ({{Nécessite un redémarrage du démon}})
+		<div class="row">
+		    <label class="col-sm-3 col-md-5 col-lg-4 control-label">
+			{{Debug étendu}}
+			<sup><i class="fas fa-question-circle" title="{{Niveau debug étendu pour le démon (très verbeux)}}"></i></sup>
+		    </label>
+		    <span class="col-xs-12 col-sm-9 col-md-7 col-lg-8" style="padding:0 !important"><input class="configKey form-control" type="checkbox" data-l1key="extendedDebug"/> ({{Nécessite un redémarrage du démon}})</span>
+		</div>
+	    </fieldset>
 
-      </div> <!-- partie gauche -->
+	</div> <!-- partie gauche -->
 
+	<div class='col-md-6 col-sm-12'> <!-- partie droite -->
+	    <legend><i class="fas fa-shield-alt"></i> {{Sécurité}}:</legend>
+	    <fieldset>
+		<div class="row">
+		    <label class="col-sm-3 col-md-5 col-lg-4 control-label">
+			{{Log non sécurisés}}
+			<sup><i class="fas fa-question-circle" title="{{Passwords et autres données sensibles en clair}}"></i></sup>
+		    </label>
+			    <span class="col-xs-12 col-sm-9 col-md-7 col-lg-8" style="padding:0 !important"><input class="configKey form-control" type="checkbox" data-l1key="unsecurelog"/></span>
+		</div>
+	    </fieldset>
+	</div> <!-- partie droite -->
     </div>
-  </fieldset>
 </form>
 
