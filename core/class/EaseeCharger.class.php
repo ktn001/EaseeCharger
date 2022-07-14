7e54ba2d83db5ba8492fb6494c8308cc127575d2
ktn <36457236+ktn001@users.noreply.github.com>
2022-07-14 10:50:47 +0200

    Update EaseeCharger.class.php

--- core/class/EaseeCharger.class.php
+++ core/class/EaseeCharger.class.php
@@ -219,15 +219,15 @@ class EaseeCharger extends eqLogic {
 		    'cable_lock' => [
 			'template' => 'cable_lock',
 			'replace' => [
-			    '#_icon_on_#' => '<i class=\'icon_green icon jeedom-lock-ferme\'><i>',
-			    '#_icon_off_#' => '<i class=\'icon_orange icon jeedom-lock-ouvert\'><i>'
+			    '#_icon_on_#' => '<i class=\'icon_green icon jeedom-lock-ferme\'><</i>',
+			    '#_icon_off_#' => '<i class=\'icon_orange icon jeedom-lock-ouvert\'></i>'
 			]
 		    ],
 		    'paused' => [
 			'template' => 'tmplicon',
 			'replace' => [
-			    '#_icon_off_#' => '000000',
-			    '#_icon_on_#' => '111111'
+			    '#_icon_off_#' => '<i class=\'icon fas fa-play\'></i>',
+			    '#_icon_on_#' => '<i class=\'icon fas fa-pause\'></i>'
 			]
 		    ]
 		]
