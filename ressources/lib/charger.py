b289a78bfbfdd44953734bf1963b376f8508969f
Christian <chris@jeedomide.khome.local>
2023-07-01 00:53:31 +0200

    nouvelle url pour signalr

--- ressources/lib/charger.py
+++ ressources/lib/charger.py
@@ -105,7 +105,7 @@ class Charger():
         self.lastMessage = None
         self.nbSignalrRestart = 0
         self.nbWatcherRestart = 0
-        url = "https://api.easee.com/hubs/chargers"
+        url = "https://streams.easee.com/hubs/chargers"
         options = {'access_token_factory': self.getToken}
 
         self.connection = HubConnectionBuilder()\
